188bafba296c58cba030d74774f81f3acea9da65
Bruno Henrique <130290196+bruno-hsl@users.noreply.github.com>
2023-08-24 19:13:37 -0300

    Add files via upload

--- exercicio7.c
+++ exercicio7.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
 main(){
-    
+    //declaracao 
+    int ano, mes, dia, TotalEmDias;
+
+    printf("\nDigite sua data de nascimento");
+    printf("Ano: ");
+    scanf("%d",&ano);
+    printf("Mes: ");
+    scanf("%d", & mes);
+    printf("Dia: ");
+    scanf("%d", &dia);
+
+    //operacao
+    TotalEmDias = ((2023-ano)*365) + (mes*30) + dia;
+ 
+    //saida
+    printf("%d", TotalEmDias);
+
+
     return 0;
 }
